1659eb8a8afdda001167406bbf962534ad016fe2
gabri <gabri@ASUSNOTEBOOK>
2022-10-24 19:45:31 -0300

    Adicionar arquivos de projeto.

---
+++ Vetor_1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Vetor_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstVetor = new System.Windows.Forms.ListBox();
+            this.btnVetor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstVetor
+            // 
+            this.lstVetor.FormattingEnabled = true;
+            this.lstVetor.ItemHeight = 15;
+            this.lstVetor.Location = new System.Drawing.Point(31, 37);
+            this.lstVetor.Name = "lstVetor";
+            this.lstVetor.Size = new System.Drawing.Size(252, 244);
+            this.lstVetor.TabIndex = 0;
+            // 
+            // btnVetor
+            // 
+            this.btnVetor.Location = new System.Drawing.Point(30, 287);
+            this.btnVetor.Name = "btnVetor";
+            this.btnVetor.Size = new System.Drawing.Size(253, 26);
+            this.btnVetor.TabIndex = 1;
+            this.btnVetor.Text = "Vetor";
+            this.btnVetor.UseVisualStyleBackColor = true;
+            this.btnVetor.Click += new System.EventHandler(this.btnVetor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 330);
+            this.Controls.Add(this.btnVetor);
+            this.Controls.Add(this.lstVetor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox lstVetor;
+        private Button btnVetor;
+    }
+}
---
+++ Vetor_1/Form1.cs
@@ -0,0 +1,20 @@
+namespace Vetor_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVetor_Click(object sender, EventArgs e)
+        {
+            //Declaração de um vetor de 10 posições
+
+            int[] vetor = new int[10];
+            int i; //Variável i para o índice
+
+
+        }
+    }
+}
